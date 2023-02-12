a39d48103c24e9a77ef366e06f54e9e0c52b2a22
The Atif Waheed <theatifwaheed@192.168.1.10>
2023-02-12 20:18:05 +0500

    CounterControllerGetX

--- lib/Screens/CounterPage/counter_page.dart
+++ lib/Screens/CounterPage/counter_page.dart
@@ -8,33 +8,68 @@ class CounterPageGetX extends StatelessWidget {
   Widget build(BuildContext context) {
     final counterController = CounterController();
     return Scaffold(
-
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: counterController.decrementCounter,
+                child: Center(
+                  child: Icon(
+                    Icons.remove,
+                    size: 20.sp,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20.r,
+              ),
+              FloatingActionButton(
+                onPressed: counterController.incrementCounter,
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    size: 20.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: RichText(
-              overflow: TextOverflow.clip,
-              textAlign: TextAlign.center,
-              textDirection: TextDirection.ltr,
-              maxLines: 3,
-              textScaleFactor: 1,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Value is: ",
-                    style: poppinsFont.copyWith(
-                      fontSize: 22.sp,
+            child: Obx(
+              () => RichText(
+                overflow: TextOverflow.clip,
+                textAlign: TextAlign.center,
+                textDirection: TextDirection.ltr,
+                maxLines: 3,
+                textScaleFactor: 1,
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Value is: ",
+                      style: poppinsFont.copyWith(
+                        fontSize: 22.sp,
+                      ),
                     ),
-                  ),
-                  TextSpan(
-                    text: "${counterController.counter}",
-                    style: poppinsFont.copyWith(
-                      fontSize: 30.sp,
+                    TextSpan(
+                      text: "${counterController.counter}",
+                      style: poppinsFont.copyWith(
+                        fontSize: 38.sp,
+                        color: Colors.green,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
